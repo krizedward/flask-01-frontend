a7adcb5fef3878e98cce718a961082f0a025000e
Edward Kristian Mangare, S.Kom. <37824502+krizedward@users.noreply.github.com>
2024-07-03 11:29:00 +0700

    frontend v.2.1
    update accounting

--- app/f03_accounting/views.py
+++ app/f03_accounting/views.py
@@ -1,12 +1,14 @@
 from flask import render_template, jsonify, session, request, redirect, url_for
 from ..mysql.models import get_all_users, add_user, get_user_by_id, delete_user
+from ..mysql.models import get_data_accounting, add_data_accounting, get_accounting_by_id, delete_data_accounting
 from . import f03_accounting
 
 # index
 @f03_accounting.route('/accounting', methods=['GET'])
 def index_accounting():
-    users = get_all_users()
-    return render_template('f03_accounting/index.html', users=users)
+    # users = get_all_users()
+    data = get_data_accounting()
+    return render_template('f03_accounting/index.html', users=data)
 
 # create
 @f03_accounting.route('/accounting/create', methods=['GET','POST'])
@@ -14,16 +16,21 @@ def create_accounting():
     if request.method == 'GET':
         return render_template('f03_accounting/create.html')
     elif request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        add_user(username, email)
+        # username = request.form['username']
+        # email = request.form['email']
+        date = request.form['date']
+        amount = request.form['amount']
+        deskripsi = request.form['deskripsi']
+        # add_user(username, email)
+        add_data_accounting(date, amount, deskripsi)
         return redirect('/accounting')
     
 # update
 @f03_accounting.route('/accounting/<accounting_id>/edit', methods=['GET','POST'])
 def edit_accounting(accounting_id):
     if request.method == 'GET':
-        user = get_user_by_id(accounting_id)
+        # user = get_user_by_id(accounting_id)
+        user = get_accounting_by_id(accounting_id)
         if user:
             return render_template('f03_accounting/edit.html', user=user)
         else:
@@ -34,9 +41,10 @@ def edit_accounting(accounting_id):
 # delete
 @f03_accounting.route('/accounting/<accounting_id>/delete', methods=['GET'])
 def delete_accounting(accounting_id):
-    user = get_user_by_id(accounting_id)
+    user = get_accounting_by_id(accounting_id)
+    
     if user:
-        delete_user(accounting_id)
+        delete_data_accounting(accounting_id)
         return redirect('/accounting')
     else:
         return 'User not found', 404
--- app/mysql/models.py
+++ app/mysql/models.py
@@ -29,3 +29,44 @@ def delete_user(user_id):
     cursor.execute('DELETE FROM users WHERE id = %s', (user_id,))
     db.commit()
     cursor.close()
+
+# accounting
+def get_data_accounting():
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM accounting')
+    data = cursor.fetchall()
+    cursor.close()
+    return data
+
+def add_data_accounting(transaction_date, transaction_amount, transaction_description):
+    db = get_db()
+    cursor = db.cursor()
+    try:
+        cursor.execute('INSERT INTO accounting (transaction_date, transaction_amount, transaction_description) VALUES (%s, %s, %s)', (transaction_date, transaction_amount, transaction_description))
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        print(f"Error: {e}")
+    finally:
+        cursor.close()
+
+def get_accounting_by_id(get_id):
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM accounting WHERE id = %s', (get_id,))
+    data = cursor.fetchone()
+    cursor.close()
+    return data
+
+def delete_data_accounting(get_id):
+    db = get_db()
+    cursor = db.cursor()
+    try:
+        cursor.execute('DELETE FROM accounting WHERE id = %s', (get_id,))
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        print(f"Error: {e}")
+    finally:
+        cursor.close()
